c178c478c27c3bbef7cc87a9588bde0e810bd34d
Antonio Fialho <80779070+AntonioFialhoSN@users.noreply.github.com>
2023-06-01 22:24:00 -0300

    3
    3

--- UDP/clienteUDP.py
+++ UDP/clienteUDP.py
@@ -1,35 +1,19 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <arpa/inet.h>
-#include <sys/socket.h>
+import socket
 
-#define PORT 12345
+# Definir informações do servidor
+host = '192.168.0.37'  # Endereço IP do servidor
+port = 12345       # Porta do servidor
 
-int main() {
-    struct sockaddr_in server_address;
-    int client_socket, bytes_sent;
-    char buffer[1024];
+# Criar um objeto de socket UDP
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-    // Criação do socket
-    client_socket = socket(AF_INET, SOCK_DGRAM, 0);
+# Enviar uma mensagem para o servidor
+message = 'Olá, servidor!'
+client_socket.sendto(message.encode(), (host, port))
 
-    // Configuração do endereço do servidor
-    server_address.sin_family = AF_INET;
-    server_address.sin_port = htons(PORT);
-    server_address.sin_addr.s_addr = inet_addr("127.0.0.1");
+# Receber a resposta do servidor
+response, server_address = client_socket.recvfrom(1024)
+print('Resposta do servidor:', response.decode())
 
-    // Envio da mensagem para o servidor
-    char message[] = "Olá, servidor!";
-    sendto(client_socket, message, strlen(message), 0, (struct sockaddr*)&server_address, sizeof(server_address));
-
-    // Recebimento da resposta do servidor
-    bytes_sent = recvfrom(client_socket, buffer, sizeof(buffer), 0, NULL, NULL);
-    buffer[bytes_sent] = '\0';
-    printf("Resposta do servidor: %s\n", buffer);
-
-    // Fechamento do socket
-    close(client_socket);
-
-    return 0;
-}
+# Fechar o socket do cliente
+client_socket.close()
--- UDP/serverUDP.py
+++ UDP/serverUDP.py
@@ -1,43 +1,22 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <arpa/inet.h>
-#include <sys/socket.h>
+import socket
 
-#define PORT 12345
+# Definir informações do servidor
+host = '192.168.0.37'  # Endereço IP do servidor
+port = 12345       # Porta para escutar as mensagens
 
-int main() {
-    struct sockaddr_in server_address, client_address;
-    int server_socket, bytes_received, client_address_length;
-    char buffer[1024];
+# Criar um objeto de socket UDP
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-    // Criação do socket
-    server_socket = socket(AF_INET, SOCK_DGRAM, 0);
+# Vincular o socket ao endereço e porta
+server_socket.bind((host, port))
 
-    // Configuração do endereço do servidor
-    server_address.sin_family = AF_INET;
-    server_address.sin_port = htons(PORT);
-    server_address.sin_addr.s_addr = INADDR_ANY;
+print('Servidor UDP esperando por mensagens...')
 
-    // Vinculação do socket ao endereço e porta
-    bind(server_socket, (struct sockaddr*)&server_address, sizeof(server_address));
+# Aguardar por mensagens do cliente
+while True:
+    message, client_address = server_socket.recvfrom(1024)
+    print('Mensagem recebida do cliente:', message.decode())
 
-    printf("Servidor UDP esperando por mensagens...\n");
-
-    while (1) {
-        // Recebimento da mensagem do cliente
-        client_address_length = sizeof(client_address);
-        bytes_received = recvfrom(server_socket, buffer, sizeof(buffer), 0, (struct sockaddr*)&client_address, &client_address_length);
-        buffer[bytes_received] = '\0';
-        printf("Mensagem recebida do cliente: %s\n", buffer);
-
-        // Processamento da mensagem recebida
-        char response[] = "Olá, cliente! Recebi sua mensagem.";
-        sendto(server_socket, response, strlen(response), 0, (struct sockaddr*)&client_address, sizeof(client_address));
-    }
-
-    // Fechamento do socket
-    close(server_socket);
-
-    return 0;
-}
+    # Processar a mensagem recebida
+    response = 'Olá, cliente! Recebi sua mensagem.'
+    server_socket.sendto(response.encode(), client_address)
